f2bd8184b409268961c6b8d49bd5297e0460cc54
Praveen Gupta <praveenguptaa8@gmail.com>
2025-01-18 22:52:15 +0530

    Added Login Functionality

--- client/src/pages/Register.jsx
+++ client/src/pages/Register.jsx
@@ -15,6 +15,12 @@ const Register = () => {
     confirmPassword: "",
   })
 
+   useEffect(() => {
+      if(localStorage.getItem('chat-app-user')){
+        navigate('/');
+      }
+    }, [])
+
   const handleSubmit = async (e) => {
     e.preventDefault();
     if(handlevalidation()){
