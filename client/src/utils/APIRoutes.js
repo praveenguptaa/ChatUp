61037efeb53ac29d0753aaa23bf4c0b7046173e7
Praveen Gupta <praveenguptaa8@gmail.com>
2025-01-26 10:51:48 +0530

    Updated

--- client/src/utils/APIRoutes.js
+++ client/src/utils/APIRoutes.js
@@ -1,4 +1,4 @@
-export const host = "http://localhost:4000";
+export const host = "https://chatup-backend-39cx.onrender.com";
 export const registerRoute = `${host}/api/auth/register`;
 
 export const loginRoute = `${host}/api/auth/login`;
--- server/index.js
+++ server/index.js
@@ -29,7 +29,7 @@ const server = app.listen(process.env.PORT, ()=>{
 
 const io = socket(server, {
     cors: {
-        origin: "http://localhost:5173",
+        origin: "beamish-otter-61d5eb.netlify.app",
         credentials: true,
     },
 });
